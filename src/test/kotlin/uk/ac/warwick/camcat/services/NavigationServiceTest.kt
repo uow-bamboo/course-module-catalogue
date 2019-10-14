e1e14bbfcfe802148f97774f907d8d458852815a
Alec Cursley <a.cursley@warwick.ac.uk>
2019-10-14 15:47:57 +0100

    Use embedded DB for all tests that use the Spring context

--- src/test/kotlin/uk/ac/warwick/camcat/services/NavigationServiceTest.kt
+++ src/test/kotlin/uk/ac/warwick/camcat/services/NavigationServiceTest.kt
@@ -2,21 +2,15 @@ package uk.ac.warwick.camcat.services
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.test.context.support.WithAnonymousUser
 import org.springframework.security.test.context.support.WithMockUser
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import uk.ac.warwick.camcat.context.ContextTest
 import uk.ac.warwick.camcat.system.security.Role
 
-@RunWith(SpringRunner::class)
-@ActiveProfiles("test")
-@SpringBootTest
-class NavigationServiceTest {
+class NavigationServiceTest : ContextTest() {
   @Autowired
   private lateinit var navigationService: NavigationService
 
--- src/test/kotlin/uk/ac/warwick/camcat/templates/helpers/NavTest.kt
+++ src/test/kotlin/uk/ac/warwick/camcat/templates/helpers/NavTest.kt
@@ -2,20 +2,14 @@ package uk.ac.warwick.camcat.templates.helpers
 
 import freemarker.template.Configuration
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit4.SpringRunner
+import uk.ac.warwick.camcat.context.ContextTest
 import uk.ac.warwick.camcat.services.NavigationDropdown
 import uk.ac.warwick.camcat.services.NavigationPage
 import uk.ac.warwick.camcat.services.NavigationPresenter
 import uk.ac.warwick.camcat.templates.FreeMarkerRendering
 
-@RunWith(SpringRunner::class)
-@ActiveProfiles("test")
-@SpringBootTest
-class NavTest : FreeMarkerRendering {
+class NavTest : ContextTest(), FreeMarkerRendering {
   @Autowired
   override lateinit var configuration: Configuration
 
@@ -27,23 +21,23 @@ class NavTest : FreeMarkerRendering {
     val model = mapOf(
       "navigation" to NavigationPresenter(
         "/", listOf(
-        NavigationPage("Home", "/"),
-        NavigationDropdown(
-          "Dropdown", "/dropdown", listOf(
-          NavigationPage("Item one", "/dropdown/one"),
-          NavigationPage("Item two", "/dropdown/two")
-        )
-        ),
-        NavigationPage(
-          "Parent page", "/parent", listOf(
+          NavigationPage("Home", "/"),
+          NavigationDropdown(
+            "Dropdown", "/dropdown", listOf(
+              NavigationPage("Item one", "/dropdown/one"),
+              NavigationPage("Item two", "/dropdown/two")
+            )
+          ),
           NavigationPage(
-            "Child page", "/parent/child", listOf(
-            NavigationPage("Sub-child page", "/parent/child/child")
-          )
+            "Parent page", "/parent", listOf(
+              NavigationPage(
+                "Child page", "/parent/child", listOf(
+                  NavigationPage("Sub-child page", "/parent/child/child")
+                )
+              )
+            )
           )
         )
-        )
-      )
       )
     )
 
