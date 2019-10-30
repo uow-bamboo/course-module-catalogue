7fca9751259cf4416263aae49c39d27e45288186
Alec Cursley <a.cursley@warwick.ac.uk>
2019-10-30 10:41:46 +0000

    CAMCAT-56 Add presenter for module information

--- src/main/kotlin/uk/ac/warwick/camcat/controllers/api/ModulesController.kt
+++ src/main/kotlin/uk/ac/warwick/camcat/controllers/api/ModulesController.kt
@@ -1,9 +1,13 @@
 package uk.ac.warwick.camcat.controllers.api
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import uk.ac.warwick.camcat.presenters.ModulePresenter
+import uk.ac.warwick.camcat.presenters.ModulePresenterFactory
 import uk.ac.warwick.camcat.sits.entities.Module
 import uk.ac.warwick.camcat.sits.entities.ModuleDescription
 import uk.ac.warwick.camcat.sits.entities.ModuleOccurrence
@@ -20,7 +24,10 @@ class ModulesController(private val moduleService: ModuleService) {
 
 @RestController
 @RequestMapping("/api/v1/modules/{moduleCode}/{academicYear}", produces = ["application/json"])
-class ModuleYearsController(private val moduleService: ModuleService) {
+class ModuleYearsController(
+  private val moduleService: ModuleService,
+  private val modulePresenterFactory: ModulePresenterFactory
+) {
   @GetMapping
   fun occurrences(@PathVariable moduleCode: String, @PathVariable academicYear: AcademicYear): Collection<ModuleOccurrence> =
     moduleService.findOccurrences(moduleCode, academicYear)
@@ -28,4 +35,15 @@ class ModuleYearsController(private val moduleService: ModuleService) {
   @GetMapping("/descriptions")
   fun descriptions(@PathVariable moduleCode: String, @PathVariable academicYear: AcademicYear): Collection<ModuleDescription> =
     moduleService.findDescriptions(moduleCode, academicYear)
+
+  @GetMapping("/present")
+  fun presentModule(@PathVariable moduleCode: String, @PathVariable academicYear: AcademicYear): ModulePresenter {
+    val module = moduleService.findByModuleCode(moduleCode) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+
+    return modulePresenterFactory.build(
+      module,
+      moduleService.findOccurrences(moduleCode, academicYear),
+      moduleService.findDescriptions(moduleCode, academicYear)
+    )
+  }
 }
---
+++ src/main/kotlin/uk/ac/warwick/camcat/presenters/UserPresenter.kt
@@ -0,0 +1,23 @@
+package uk.ac.warwick.camcat.presenters
+
+import org.springframework.stereotype.Component
+import uk.ac.warwick.userlookup.AnonymousUser
+import uk.ac.warwick.userlookup.User
+import uk.ac.warwick.userlookup.UserLookup
+
+@Component
+class UserPresenterFactory(private val userLookup: UserLookup) {
+  fun buildFromPersonnelCode(personnelCode: String): UserPresenter = UserPresenter(
+    if (personnelCode.length == 9) {
+      val universityId = personnelCode.drop(2)
+
+      userLookup.getUserByWarwickUniId(universityId)
+    } else AnonymousUser()
+  )
+}
+
+class UserPresenter(user: User) {
+  val universityId = user.warwickId
+  val name = user.fullName
+  val email = user.email
+}
