f43e9d24e67f513609e7705c9b5dd9f86482a457
Alec Cursley <a.cursley@warwick.ac.uk>
2019-11-07 17:08:12 +0000

    CAMCAT-84 Spring Boot 2.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
-  id("org.springframework.boot") version "2.2.0.RELEASE"
+  id("org.springframework.boot") version "2.2.1.RELEASE"
   id("io.spring.dependency-management") version "1.0.8.RELEASE"
   kotlin("jvm") version "1.3.50"
   kotlin("plugin.spring") version "1.3.50"
