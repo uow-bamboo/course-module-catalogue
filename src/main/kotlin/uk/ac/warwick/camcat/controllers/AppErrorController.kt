acea446c3136fd1d00d772e07f77a2e93c49be98
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-11-08 12:18:44 +0000

    Fix AppErrorController to show the correct exception & stack trace

--- src/main/kotlin/uk/ac/warwick/camcat/controllers/AppErrorController.kt
+++ src/main/kotlin/uk/ac/warwick/camcat/controllers/AppErrorController.kt
@@ -1,38 +1,42 @@
 package uk.ac.warwick.camcat.controllers
 
-import org.apache.commons.lang3.exception.ExceptionUtils
+import org.springframework.boot.web.servlet.error.ErrorAttributes
 import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.WebAttributes
 import org.springframework.security.web.csrf.CsrfException
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.ModelAndView
 import uk.ac.warwick.camcat.system.ServiceException
 import uk.ac.warwick.camcat.system.security.Authority
 import uk.ac.warwick.camcat.system.security.WarwickAuthentication
 import javax.servlet.RequestDispatcher
-import javax.servlet.http.HttpServletRequest
 
 @Controller
-class AppErrorController : ErrorController {
+class AppErrorController(
+  private val errorAttributes: ErrorAttributes
+) : ErrorController {
   @RequestMapping("/error")
   fun error(
-    request: HttpServletRequest,
-    exception: Exception,
+    request: WebRequest,
     auth: Authentication?
   ): ModelAndView {
-    if (request.getAttribute(WebAttributes.ACCESS_DENIED_403) is CsrfException) {
+    if (request.getAttribute(WebAttributes.ACCESS_DENIED_403, RequestAttributes.SCOPE_REQUEST) is CsrfException) {
       return ModelAndView("errors/csrf")
     }
 
-    if (exception is ServiceException) {
-      return ModelAndView("errors/500", mapOf("message" to exception.message))
+    val throwable = errorAttributes.getError(request)
+
+    if (throwable is ServiceException) {
+      return ModelAndView("errors/500", mapOf("error" to throwable.message))
     }
 
     val warwickAuth = auth as? WarwickAuthentication
 
-    return when (request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)) {
+    return when (request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE, RequestAttributes.SCOPE_REQUEST)) {
       403 -> ModelAndView(
         "errors/403",
         mapOf(
@@ -44,9 +48,8 @@ class AppErrorController : ErrorController {
       400 -> ModelAndView("errors/400")
       else -> ModelAndView(
         "errors/500",
-        if (auth?.authorities?.contains(Authority.sysadmin) == true)
-          mapOf("stackTrace" to ExceptionUtils.getStackTrace(exception))
-        else mapOf()
+        // TODO In Kotlin land this will always be a UndeclaredThrowableException with the cause being the actual thing, maybe we could teach ErrorAttributes to strip that out
+        errorAttributes.getErrorAttributes(request, auth?.authorities?.contains(Authority.sysadmin) == true)
       )
     }
   }
