d48a6baa1500c8e62c80b5071182751fc883aa04
Alec Cursley <a.cursley@warwick.ac.uk>
2019-12-12 15:47:17 +0000

    CAMCAT-113 Trigger module index via UI

--- src/main/kotlin/uk/ac/warwick/camcat/controllers/SysadminController.kt
+++ src/main/kotlin/uk/ac/warwick/camcat/controllers/SysadminController.kt
@@ -1,9 +1,14 @@
 package uk.ac.warwick.camcat.controllers
 
+import org.quartz.JobKey.jobKey
+import org.quartz.Scheduler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.View
+import org.springframework.web.servlet.view.RedirectView
 import uk.ac.warwick.camcat.system.security.Role
 import javax.annotation.security.RolesAllowed
 
@@ -13,4 +18,18 @@ import javax.annotation.security.RolesAllowed
 class SysadminController {
   @GetMapping
   fun home() = ModelAndView("sysadmin/home")
+}
+
+@Controller
+@RequestMapping("/sysadmin/index/modules")
+@RolesAllowed(Role.sysadmin)
+class TriggerIndexModulesJobController(
+  private val scheduler: Scheduler
+) {
+  @PostMapping
+  fun trigger(): View {
+    scheduler.triggerJob(jobKey("IndexModulesJob", "Indexing"))
+
+    return RedirectView("/sysadmin")
+  }
 }
