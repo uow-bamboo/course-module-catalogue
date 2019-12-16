db456757ecbb17f73b116d5973720ec4a6cefeb2
Kai Lan <K.Lan@warwick.ac.uk>
2019-12-16 16:03:00 +0000

    Merge pull request #53 in CAMCAT/app from CAMCAT-131 to develop
    Squashed commit of the following:
    
    commit 4e226a149d6e9b2622ce8daec2a82d6b3b132b39
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 15:59:10 2019 +0000
    
        CAMCAT-131 tidy up
    
    commit e676e788a32f69ed148f3dd4f91ab38e45f064f1
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 15:50:18 2019 +0000
    
        CAMCAT-131 tidy up
    
    commit 1d001b30fbf3eaf655068bef4e02cf94dfc9dcfd
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 14:55:37 2019 +0000
    
        CAMCAT-131 assert the correct filter value is selected (which is dcs department in this case)
    
    commit 2339c7960b607b23fce697a3c43456f81ca21328
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 14:41:13 2019 +0000
    
        CAMCAT-131 tidy up
    
    commit bb6eefbe9a5b63e9fdf47ae77a448ac9f328c5b8
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 14:40:05 2019 +0000
    
        CAMCAT-131 better test to ensure that the linked page is 100% for the asked department
    
    commit b3bb1523fc84de698e2cdb61f075f5cc39016710
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 12:27:34 2019 +0000
    
        CAMCAT-131 correct links to the page
    
    commit 2cfb3652e2abdde1425b8c2ad249eddb9ef15765
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Mon Dec 16 12:27:09 2019 +0000
    
        CAMCAT-131 integration test / confirming the issue

--- src/test/kotlin/uk/ac/warwick/camcat/mvc/HomeIntegrationTest.kt
+++ src/test/kotlin/uk/ac/warwick/camcat/mvc/HomeIntegrationTest.kt
@@ -1,6 +1,10 @@
 package uk.ac.warwick.camcat.mvc
 
+import com.gargoylesoftware.htmlunit.html.*
 import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.hasSize
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.springframework.security.test.context.support.WithAnonymousUser
 import org.springframework.security.test.context.support.WithMockUser
@@ -23,5 +27,48 @@ class HomeIntegrationTest : IntegrationTest() {
     mvc.get("/").andExpect {
       status { isOk }
     }
+  }
+
+  @Test
+  @WithMockUser(roles = [Role.user])
+  fun linkToViewByDepartmentPage() {
+    var page: HtmlPage = webClient.getPage("http://localhost")
+    val links = page.anchors.filter { it.textContent.trim() == "Computer Science" }
+    assertThat(links, hasSize(1))
+    page = links.first().click()
+
+    var moduleRows = emptyList<HtmlTableRow>()
+    var csModuleRows = emptyList<HtmlTableRow>()
+    var selectedFilterOptions = emptyList<HtmlOption>()
+
+    fun getPageResult() {
+      moduleRows = page.getByXPath<HtmlTableRow>("//tr[contains(@class, 'module')]")
+      csModuleRows = moduleRows.filter { it.getElementsByTagName("td").last().textContent.trim() == "DCS" }
+      selectedFilterOptions = page.getByXPath<HtmlOption>("//option[@selected]")
+    }
+
+    // 31 dcs modules across 2 pages
+    // ensure entire first page is DCS modules
+    getPageResult()
+    assertThat(moduleRows, hasSize(20))
+    assertThat(csModuleRows, hasSize(moduleRows.size))
+    assertThat(selectedFilterOptions, hasSize(1))
+    assertThat(selectedFilterOptions.first().textContent, equalTo("DCS"))
+
+    fun getLastPage() {
+      val pageButtons = page
+        .getByXPath<HtmlUnorderedList>("//ul[contains(@class, 'pagination')]")
+        .first().getByXPath<HtmlButton>("//button")
+      val lastPageNumber = pageButtons[pageButtons.size - 2].valueAttribute
+      page = webClient.getPage("http://localhost/modules/?departments=CS&page=${lastPageNumber}}")
+    }
+
+    // ensure 2nd page is also entirely DCS
+    // pageButton.click() does not work, hence we are manually navigating to 2nd page
+    getLastPage()
+    getPageResult()
+    assertThat(csModuleRows, hasSize(moduleRows.size))
+    assertThat(selectedFilterOptions, hasSize(1))
+    assertThat(selectedFilterOptions.first().textContent, equalTo("DCS"))
   }
 }
